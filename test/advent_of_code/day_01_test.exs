0b1d325d64c7958372b632053ab9e441192510ab
Andres Martin <monkeyandres@protonmail.com>
2021-12-01 09:24:24 +0100

    Initial commit

---
+++ test/advent_of_code/day_01_test.exs
@@ -0,0 +1,22 @@
+defmodule AdventOfCode.Day01Test do
+  use ExUnit.Case
+
+  import AdventOfCode.Day01
+  import AdventOfCode.Input
+
+  @tag :skip
+  test "part1" do
+    input = nil
+    result = part1(input)
+
+    assert result
+  end
+
+  @tag :skip
+  test "part2" do
+    input = nil
+    result = part2(input)
+
+    assert result
+  end
+end
