45e17de06ef07bdc15cbac70b56326f3acbd0fe1
Andres Martin <monkeyandres@protonmail.com>
2021-12-02 09:12:22 +0100

    ✨ Solve day 2 part 1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,5 +1,20 @@
 defmodule AdventOfCode.Day02 do
-  def part1(args) do
+  def part1(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.map(fn direction ->
+      [direction, value] = String.split(direction, " ", trim: true)
+      [direction, String.to_integer(value)]
+    end)
+    |> Enum.reduce(%{horizontal: 0, depth: 0}, fn direction, acc ->
+      case direction do
+        ["forward", x] -> %{horizontal: acc.horizontal + x, depth: acc.depth}
+        ["up", x] -> %{horizontal: acc.horizontal, depth: acc.depth - x}
+        ["down", x] -> %{horizontal: acc.horizontal, depth: acc.depth + x}
+        _ -> acc
+      end
+    end)
+    |> Map.values()
+    |> Enum.product()
   end
 
   def part2(args) do
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,12 +3,27 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @example_input "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
 
-    assert result
+  @final_input AdventOfCode.Input.get!(2, 2021)
+
+  describe "part 1" do
+    test "example input" do
+      result = part1(@example_input)
+
+      assert result === 150
+    end
+
+    test "part1" do
+      result = part1(@final_input)
+
+      assert result === 1_484_118
+    end
   end
 
   @tag :skip
