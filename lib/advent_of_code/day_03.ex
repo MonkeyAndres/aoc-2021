53ba7e8ca6c130763dbc379aea186713e6948bc3
Andres Martin <monkeyandres@protonmail.com>
2021-12-03 11:33:02 +0100

    ✨ Solve day 3 part 1

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,36 @@
 defmodule AdventOfCode.Day03 do
-  def part1(args) do
+  def parse_binary_diagnosis(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.map(&String.graphemes/1)
   end
 
-  def part2(args) do
+  def binary_to_dec(binaryString) do
+    Integer.parse(binaryString, 2)
+    |> elem(0)
+  end
+
+  def part1(input) do
+    submarine_rates =
+      parse_binary_diagnosis(input)
+      |> Enum.zip()
+      |> Enum.reduce(%{gamma_rate: "", epsilon_rate: ""}, fn bitsColumnTuple, acc ->
+        bitsColumn = Tuple.to_list(bitsColumnTuple)
+
+        occurrences_0 = Enum.count(bitsColumn, &(&1 == "0"))
+        occurrences_1 = Enum.count(bitsColumn, &(&1 == "1"))
+
+        if occurrences_0 < occurrences_1 do
+          Map.update!(acc, :gamma_rate, &(&1 <> "0"))
+          |> Map.update!(:epsilon_rate, &(&1 <> "1"))
+        else
+          Map.update!(acc, :gamma_rate, &(&1 <> "1"))
+          |> Map.update!(:epsilon_rate, &(&1 <> "0"))
+        end
+      end)
+
+    binary_to_dec(submarine_rates.gamma_rate) * binary_to_dec(submarine_rates.epsilon_rate)
+  end
+
+  def part2(input) do
   end
 end
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -3,12 +3,33 @@ defmodule AdventOfCode.Day03Test do
 
   import AdventOfCode.Day03
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @example_input "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
 
-    assert result
+  @final_input AdventOfCode.Input.get!(3, 2021)
+
+  describe "part 1" do
+    test "example input" do
+      result = part1(@example_input)
+
+      assert result === 198
+    end
+
+    test "final input" do
+      result = part1(@final_input)
+
+      assert result === 1_025_636
+    end
   end
 
   @tag :skip
