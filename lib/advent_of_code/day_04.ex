da88dffee0f656e01036b4ec9c0cdf37e0492d10
Andres Martin <monkeyandres@protonmail.com>
2021-12-05 16:28:28 +0100

    ✨ Solve day 4 part 1

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,5 +1,73 @@
 defmodule AdventOfCode.Day04 do
-  def part1(args) do
+  def parse_bingo_data(input) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> then(fn [draw_numbers | boards] ->
+      {
+        String.split(draw_numbers, ",", trim: true),
+        Enum.map(
+          boards,
+          fn board ->
+            String.split(board, ["\n", " "], trim: true)
+            |> Enum.chunk_every(5)
+          end
+        )
+      }
+    end)
+  end
+
+  def mark_number_in_boards(boards, draw_number) do
+    Enum.map(boards, fn board ->
+      Enum.map(board, fn row ->
+        Enum.map(row, fn number ->
+          if number === draw_number, do: "X", else: number
+        end)
+      end)
+    end)
+  end
+
+  def calculate_final_result(winning_board, current_draw) do
+    List.flatten(winning_board)
+    |> Enum.reduce(0, fn element, acc ->
+      if element != "X" do
+        acc + String.to_integer(element)
+      else
+        acc
+      end
+    end)
+    |> then(&(&1 * String.to_integer(current_draw)))
+  end
+
+  def check_winning_board(board) do
+    has_winning_row =
+      Enum.any?(
+        board,
+        fn row -> Enum.all?(row, &(&1 === "X")) end
+      )
+
+    has_winning_column =
+      Enum.zip_with(board, &Function.identity/1)
+      |> Enum.any?(fn column -> Enum.all?(column, &(&1 === "X")) end)
+
+    has_winning_row or has_winning_column
+  end
+
+  def part1(input) do
+    {draw_numbers, boards} = parse_bingo_data(input)
+
+    Enum.reduce_while(draw_numbers, boards, fn current_draw, boards ->
+      next_boards = mark_number_in_boards(boards, current_draw)
+      winning_board = Enum.find(next_boards, &check_winning_board/1)
+
+      if winning_board do
+        {
+          :halt,
+          calculate_final_result(winning_board, current_draw)
+        }
+      else
+        {:cont, next_boards}
+      end
+    end)
   end
 
   def part2(args) do
