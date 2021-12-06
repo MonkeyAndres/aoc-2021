e3059ea1a8e875418f5c54095595ac9fc273b174
Andres Martin <monkeyandres@protonmail.com>
2021-12-06 14:22:40 +0100

    ✨ Solve day 5

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,78 @@
 defmodule AdventOfCode.Day05 do
-  def part1(args) do
+  def parse_input(input) do
+    input
+    |> String.split(["\n", " -> ", ","], trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(4)
   end
 
-  def part2(args) do
+  def part1(input) do
+    parse_input(input)
+    |> Enum.reduce(%{}, fn [x1, y1, x2, y2], acc ->
+      cond do
+        x1 === x2 ->
+          Range.new(y1, y2)
+          |> Enum.reduce(acc, fn y, next_acc ->
+            Map.update(next_acc, "#{x1},#{y}", 1, &(&1 + 1))
+          end)
+
+        y1 === y2 ->
+          Range.new(x1, x2)
+          |> Enum.reduce(acc, fn x, next_acc ->
+            Map.update(next_acc, "#{x},#{y1}", 1, &(&1 + 1))
+          end)
+
+        true ->
+          acc
+      end
+    end)
+    |> Map.values()
+    |> Enum.count(&(&1 > 1))
+  end
+
+  def get_slope({x1, y1}, {x2, y2}) do
+    (y2 - y1) / (x2 - x1)
+  end
+
+  def is_diagonal_line?(x1, y1, x2, y2) do
+    abs(get_slope({x1, y1}, {x2, y2})) === 1.0
+  end
+
+  def part2(input) do
+    parse_input(input)
+    |> Enum.reduce(%{}, fn [x1, y1, x2, y2], acc ->
+      cond do
+        x1 === x2 ->
+          Range.new(y1, y2)
+          |> Enum.reduce(acc, fn y, next_acc ->
+            Map.update(next_acc, "#{x1},#{y}", 1, &(&1 + 1))
+          end)
+
+        y1 === y2 ->
+          Range.new(x1, x2)
+          |> Enum.reduce(acc, fn x, next_acc ->
+            Map.update(next_acc, "#{x},#{y1}", 1, &(&1 + 1))
+          end)
+
+        is_diagonal_line?(x1, y1, x2, y2) ->
+          x_values =
+            Range.new(x1, x2)
+            |> Enum.to_list()
+
+          y_values =
+            Range.new(y1, y2)
+            |> Enum.to_list()
+
+          List.zip([x_values, y_values])
+          |> Enum.reduce(acc, fn {x, y}, next_acc ->
+            Map.update(next_acc, "#{x},#{y}", 1, &(&1 + 1))
+          end)
+
+        true ->
+          acc
+      end
+    end)
+    |> Map.values()
+    |> Enum.count(&(&1 > 1))
   end
 end
--- test/advent_of_code/day_05_test.exs
+++ test/advent_of_code/day_05_test.exs
@@ -3,19 +3,44 @@ defmodule AdventOfCode.Day05Test do
 
   import AdventOfCode.Day05
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @example_input "0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2"
 
-    assert result
+  @final_input AdventOfCode.Input.get!(5, 2021)
+
+  describe "part 1" do
+    test "example input" do
+      result = part1(@example_input)
+
+      assert result === 5
+    end
+
+    test "final input" do
+      result = part1(@final_input)
+
+      assert result === 7269
+    end
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  describe "part 2" do
+    test "example input" do
+      result = part2(@example_input)
 
-    assert result
+      assert result === 12
+    end
+
+    test "final input" do
+      result = part2(@final_input)
+
+      assert result === 21140
+    end
   end
 end
